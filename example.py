c2f1a096a56cd3b996558552e29d5056eff67a63
Defiant7426 <pldelacruzv@gmail.com>
2024-07-08 18:46:21 -0500

    Add ask_name function to example.py

--- example.py
+++ example.py
@@ -5,6 +5,10 @@ def greet():
 def farewell():
     return "Goodbye, GitHub!"
 
+def ask_name():
+    return "What is your name?"
+
 if __name__ == "__main__":
     print(greet())
     print(farewell())
+    print(ask_name())
